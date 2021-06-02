06cad902bfe60a98da0921a2571047e606b295bd
UnderT0p <70749855+UnderT0p@users.noreply.github.com>
2021-06-02 19:58:05 +0300

    Update terra1.tf

--- terra1.tf
+++ terra1.tf
@@ -9,9 +9,9 @@ terraform {
     endpoint   = "storage.yandexcloud.net"
     bucket     = "bcommon"
     region     = "ru-central1"
-    key        = var.key
-    access_key = var.access_key
-    secret_key = var.secret_key
+    key        = "***"
+    access_key = "*********"
+    secret_key = "*********"
 
     skip_region_validation      = true
     skip_credentials_validation = true
